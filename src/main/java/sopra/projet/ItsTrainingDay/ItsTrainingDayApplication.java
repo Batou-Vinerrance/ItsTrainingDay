4823109a3380ba492588f366a58b5e7321f14a59
Bastien <bastien.dd@dbmail.com>
2020-01-08 15:54:23 +0100

    Initialisation du projet

---
+++ src/main/java/sopra/projet/ItsTrainingDay/ItsTrainingDayApplication.java
@@ -0,0 +1,13 @@
+package sopra.projet.ItsTrainingDay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsTrainingDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsTrainingDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sopra/projet/ItsTrainingDay/ItsTrainingDayApplicationTests.java
@@ -0,0 +1,13 @@
+package sopra.projet.ItsTrainingDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsTrainingDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
